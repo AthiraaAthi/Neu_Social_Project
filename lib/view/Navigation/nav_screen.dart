1771442040506e06e7517cc9260f6e66001beb78
AthiraaAthi <athiraa2612@gmail.com>
2024-05-28 10:25:52 +0530

    starting navigation screen

---
+++ lib/view/Navigation/nav_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({super.key});
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  int selectedIndex = 0;
+  List<Widget> Screens = [];
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
