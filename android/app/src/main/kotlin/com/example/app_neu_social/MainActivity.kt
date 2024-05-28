234019bae820cccb6daefb8e15fd2c364b948e35
AthiraaAthi <athiraa2612@gmail.com>
2024-05-28 09:52:26 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/app_neu_social/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_neu_social
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class NeuSocial extends StatelessWidget {
+  const NeuSocial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
